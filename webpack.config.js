3c70c21abc5ef265475c3c7ec8c124c2f5e49616
olipliche <olipliche@gmail.com>
2021-09-08 19:01:50 +0100

    Updates webpack and linters file

--- webpack.config.js
+++ webpack.config.js
@@ -3,24 +3,25 @@ const HtmlWebpackPlugin = require('html-webpack-plugin');
 
 module.exports = {
   entry: './src/index.js',
-    devServer: {
-        static: './dist',
-      },
-   plugins: [
-     new HtmlWebpackPlugin({
-        template: './src/index.html'
-   }),
+  devServer: {
+    static: './dist',
+  },
+  plugins: [
+    new HtmlWebpackPlugin({
+      template: './src/index.html',
+    }),
   ],
+  mode: 'development',
   output: {
     filename: 'main.js',
     path: path.resolve(__dirname, 'dist'),
   },
-    module: {
-        rules: [
-          {
-            test: /\.css$/i,
-            use: ['style-loader', 'css-loader'],
-          },
-        ],
+  module: {
+    rules: [
+      {
+        test: /\.css$/i,
+        use: ['style-loader', 'css-loader'],
       },
+    ],
+  },
 };

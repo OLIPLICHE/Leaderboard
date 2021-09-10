ac7d923dcca84885df30d186b71ddc4802cf135c
olipliche <olipliche@gmail.com>
2021-09-10 18:14:09 +0100

    Update api  file

--- src/api.js
+++ src/api.js
@@ -1,19 +1,21 @@
-/* eslint-disable semi */
-/* eslint-disable linebreak-style */
-// getting data from the API
-const url = 'https://us-central1-js-capstone-backend.cloudfunctions.net/api/';
-export const get = async (endPoint) => {
-  const result = await fetch(`${url}${endPoint}`);
-  return result.json();
-};
-// post data to the API
-export const post = async (endPoint, body) => {
-  const result = await fetch(`${url}${endPoint}`, {
+const nameInput = document.getElementById('name');
+const scoreInput = document.getElementById('score');
+
+const postScores = async (url) => {
+  const result = await fetch(url, {
     method: 'POST',
-    body,
+    body: JSON.stringify({
+      user: nameInput.value,
+      score: scoreInput.value,
+    }),
     headers: {
-      'content-type': 'application/json; charset=UTF-8',
+      'Content-type': 'application/json; charset=UTF-8',
     },
   });
+
+  nameInput.value = '';
+  scoreInput.value = '';
   return result.json();
 };
+
+export default postScores;
